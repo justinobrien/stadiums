7c81af00c3e8be06e65102b0ee7bcb0efe3ae3b3
justinobrien <obrien.justin1008@gmail.com>
2020-07-18 10:30:47 -0700

    revert to herocomponent back to jsx and remove slides

---
+++ src/components/heroComponent/HeroComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import SlideShow
+import LogoComponent from '../logoComponent/LogoComponent';
+import './HeroComponent.css';
+
+const HeroComponent = () => {
+  return (
+    <div className='hero'>
+      <LogoComponent />
+    </div> 
+  );
+};
+
+export default HeroComponent;
