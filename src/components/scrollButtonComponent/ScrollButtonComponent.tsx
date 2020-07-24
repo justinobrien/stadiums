73fc36f3965641f4893c3f865198739bca48bf96
justinobrien <obrien.justin1008@gmail.com>
2020-07-24 14:49:44 -0700

    auto scroll working

---
+++ src/components/scrollButtonComponent/ScrollButtonComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ScrollButtonComponent.css';
+import Image from 'react';
+
+const ScrollButtonComponent = () => {
+  return (
+    <button className='scrollButton'></button>
+  );
+};
+
+export default ScrollButtonComponent;
--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -3,16 +3,27 @@ import HeroComponent from '../../components/heroComponent/HeroComponent';
 import BeerMenuComponent from '../../components/beerMenuComponent/BeerMenuComponent';
 import SpecialEventsComponent from '../../components/specialEventsComponent/SpecialEventsComponent';
 import './LandingPage.css';
+import ScrollButtonComponent from '../../components/scrollButtonComponent/ScrollButtonComponent';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const LandingPage = () => {
   return (
     <div className="landing-page-div">
+      <Link
+          activeClass="active"
+          to="section1"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+          className="link-container"
+      > <span className="link-button"></span> </Link>
       <HeroComponent />
-      <div className="row">
+      <div className="row section1">
         <BeerMenuComponent />
         <SpecialEventsComponent />
       </div>
-      <div className="row">
+      <div className="row section">
         <SpecialEventsComponent />
         <BeerMenuComponent />
       </div>
