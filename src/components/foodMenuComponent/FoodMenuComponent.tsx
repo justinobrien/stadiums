15e3de04bcc3fa1b32bcc64b79b6260221236a58
justinobrien <obrien.justin1008@gmail.com>
2020-10-03 12:01:47 -0700

    added placeholder images for food menu and fixed button layout

--- src/components/foodMenuComponent/FoodMenuComponent.tsx
+++ src/components/foodMenuComponent/FoodMenuComponent.tsx
@@ -36,14 +36,16 @@ const FoodMenuComponent = () => {
         button3 button4
        */}
        <div className={background} onClick={() => { navigateTo()}}>
-        <div className="row">
-          <div className='btn' onClick={(e) => { setBg('breakfast-bg', e)}} onMouseOver={() => previewBg('breakfast-bg')} onMouseOut={() => restoreBg()}> breakfast </div>
-          <div className='btn' onClick={(e) => { setBg('lunch-bg', e)}} onMouseOver={() => previewBg('lunch-bg')} onMouseOut={() => restoreBg()}> lunch </div>
-        </div>
-        <div className="row">
-          <div className='btn' onClick={(e) => { setBg('dinner-bg', e)}} onMouseOver={() => previewBg('dinner-bg')} onMouseOut={() => restoreBg()}> dinner </div>
-          <div className='btn' onClick={(e) => { setBg('happy-bg', e)}} onMouseOver={() => previewBg('happy-bg')} onMouseOut={() => restoreBg()}> happy </div>
-        </div>
+         <div className='food-menu-button-wrapper'>
+          <div className='row'>
+            <div className='btn' onClick={(e) => { setBg('breakfast-bg', e)}} onMouseOver={() => previewBg('breakfast-bg')} onMouseOut={() => restoreBg()}> breakfast </div>
+            <div className='btn' onClick={(e) => { setBg('lunch-bg', e)}} onMouseOver={() => previewBg('lunch-bg')} onMouseOut={() => restoreBg()}> lunch </div>
+          </div>
+          <div className='row'>
+            <div className='btn' onClick={(e) => { setBg('dinner-bg', e)}} onMouseOver={() => previewBg('dinner-bg')} onMouseOut={() => restoreBg()}> dinner </div>
+            <div className='btn' onClick={(e) => { setBg('happy-bg', e)}} onMouseOver={() => previewBg('happy-bg')} onMouseOut={() => restoreBg()}> happy </div>
+          </div>
+         </div>
        </div>
        
     </div>
