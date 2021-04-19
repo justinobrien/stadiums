f38df04783b14b5c2185ef3c17546d404f931403
justinobrien <obrien.justin1008@gmail.com>
2021-04-19 16:38:29 -0700

    logos and links added

--- src/components/sportsComponent/SportsComponent.tsx
+++ src/components/sportsComponent/SportsComponent.tsx
@@ -5,15 +5,28 @@ import { NavLink } from 'react-router-dom';
 const SportsComponent = () => {
   return (
     <div className='sports'>
-      <a href="https://www.ufc.com/events">
-        <div className="UFC"></div>
-      </a>
-      <NavLink className="NHL" to="https://www.ufc.com/events"></NavLink>
-      <NavLink className="NBA" to="https://www.ufc.com/events"></NavLink>
-      <NavLink className="NHL" to="https://www.ufc.com/events"></NavLink>
-      <NavLink className="MLB" to="https://www.ufc.com/events"></NavLink>
-      <NavLink className="Blazers" to="https://www.ufc.com/events"></NavLink>
-      <NavLink className="Timbers" to="https://www.ufc.com/events"></NavLink>
+      <div className="row">
+        <a href="https://www.ufc.com/events">
+          <div className="UFC"></div>
+        </a>
+        <a href="https://www.nhl.com/schedule">
+          <div className="NHL"></div>
+        </a>
+        <a href="https://www.nba.com/schedule">
+          <div className="NBA"></div>
+        </a>
+      </div>
+      <div className="row">
+        <a href="https://www.nba.com/blazers/schedule">
+          <div className="Blazers"></div>
+        </a>
+        <a href="https://www.timbers.com/schedule">
+          <div className="Timbers"></div>
+        </a>
+        <a href="https://www.mlb.com/schedule">
+          <div className="MLB"></div>
+        </a>
+      </div>
     </div>
   );
 };
