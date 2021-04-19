1a4dd29aeca200cb4a8d1305c7ac5a3ee5ece157
justinobrien <obrien.justin1008@gmail.com>
2021-04-19 12:31:54 -0700

    breakfast menu mostly done

--- src/pages/food/BreakfastMenu.tsx
+++ src/pages/food/BreakfastMenu.tsx
@@ -1,25 +1,43 @@
 import React, { ReactFragment } from 'react';
 import './BreakfastMenu.scss';
 import drinks from '../../jsonfiles/breakfast/breakfast_drinks.json'; // Relative path to your File
+import breakfastpg2 from '../../jsonfiles/breakfast/breakfast_pg2.json'; // Relative path to your File
+import sides from '../../jsonfiles/breakfast/breakfast_sides.json'; // Relative path to your File
+import omolets from '../../jsonfiles/breakfast/omolets.json'; // Relative path to your File
+import skillets from '../../jsonfiles/breakfast/skillets.json'; // Relative path to your File
 
 const breakfastItems = [];
 const warning = 'Consuming raw or undercooked meats, seafood, shellfish or eggs may increase your risk of foodborne illness, especially if you have certain medical conditions';
 let breakfastMenu: Array<ReactFragment> = [];
 
+const addMenuItem = (item: any) => {
+  breakfastMenu.push(
+    <div className="drink-item">
+      <div className="row"> 
+        <div className="item-name">{item.name}</div>
+        <hr></hr>
+        <div className='price'>{item.price}</div>
+      </div>
+      <div className="item-description">{item.desc}</div>
+    </div>)
+}
+
 const BreakfastMenu = () => {
   if (breakfastMenu.length < 1) {
+    breakfastMenu.push(<div className="subtitle-1 no-margin">Omolets</div>);
+    omolets.map(d => addMenuItem(d));
+  
+    breakfastMenu.push(<div className="subtitle-1"> Traditional Breakfast </div>);
+    breakfastpg2.map(d => addMenuItem(d));
 
-    breakfastMenu.push(<div className="subtitle-1"> Drink Menu </div>);
-    drinks.map(d => breakfastMenu.push(
-      <div className="drink-item">
-        <div className="row"> 
-          <div className="item-name">{d.name}</div>
-          <hr></hr>
-          <div className='price'>{d.price}</div>
-        </div>
-        <div className="item-description">{d.desc}</div>
-      </div>)
-    );
+    breakfastMenu.push(<div className="subtitle-1"> Skillets </div>);
+    skillets.map(d => addMenuItem(d));
+
+    breakfastMenu.push(<div className="subtitle-1"> Sides </div>);
+    sides.map(d => addMenuItem(d));
+
+    breakfastMenu.push(<div className="subtitle-1">Drinks</div>);
+    drinks.map(d => addMenuItem(d));
   }
 
   return (
