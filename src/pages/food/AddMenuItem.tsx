c12b208598307c5e3fcfea557442083ea54c38d2
justinobrien <obrien.justin1008@gmail.com>
2021-04-26 17:15:18 -0700

    resizing images for menu items

--- src/pages/food/AddMenuItem.tsx
+++ src/pages/food/AddMenuItem.tsx
@@ -4,7 +4,7 @@ const addMenuItem = (arr: Array<any>, item: any) => {
     arr.push(
       <div className="menu-item">
         {item?.day?.length ? <div className="item-day subtitle-1">{item.day}</div>: null}
-        {item?.img?.length ?<div className="item-img" style={{ backgroundImage: `url(${item.img})`, backgroundSize: 'cover', width: '30vw', height: '30vh'}} /> : null}
+        {item?.img?.length ?<div className="item-img" style={{ backgroundImage: `url(${item.img})` }} /> : null}
         <div className="row">
           <div className="item-name">{item.name}</div>
           <hr></hr>
