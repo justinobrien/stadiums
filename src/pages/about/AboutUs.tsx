d7da9fb1a96ac76617fd4d52aff361956d51da06
justinobrien <obrien.justin1008@gmail.com>
2021-06-28 17:06:13 -0700

    mobile styling

--- src/pages/about/AboutUs.tsx
+++ src/pages/about/AboutUs.tsx
@@ -43,26 +43,23 @@ const AboutUs = () => {
             <div className="aboutusInfo"> Welcome to Stadiums, Milwalkie's premier sports bar where we provide a white collar experience at blue collar prices, we do this by making our food in house using high quality local ingredients and by hiring experienced, friendly wait staff.</div>
             <div className="content-wrapper">
                 <div className="content-wrapper info">
-                    <div className="contactInfo">
-                        <p>
-                            <LocationOn className="icon" fontSize="small"/>16065 SE McLoughlin Blvd <br/> Milwaukie, OR 97267 <br/>
-                            <MailOutline className="icon" fontSize="small"/>info@stadiumssportsgrill.com <br/>
-                            <Phone className="icon" fontSize="small"/>503-305-8410
-                        </p>
+                    <div className="contactInfo row">
+                        <div className="column"><LocationOn className="icon" fontSize="small"/> 16065 SE McLoughlin Blvd Milwaukie, OR 97267</div>
+                        <div className="column"><MailOutline className="icon" fontSize="small"/> info@stadiumssportsgrill.com</div>
+                        <div className="column"><Phone className="icon" fontSize="small"/> 503-305-8410</div>
                     </div>
-                    <div className="opperationInfo">
-                        <h3> HOURS OF OPERATION </h3>
-                        <p>
-                            Saturday 10AM to 11PM <br/>
-                            Sunday 10AM to 10PM <br/>
-                            Monday and Tuesday noon to 10 PM <br/>
-                            Wednesday noon to 11PM <br/>
-                            Thursday and Friday 11AM to 11PM
-                        </p>
+                    <div className="opperationInfo row">
+                        <div className="column">Mon <div>12pm-10pm</div></div>
+                        <div className="column">Tue <div>12pm-10pm</div></div>
+                        <div className="column">Wed <div>12pm-11pm</div></div>
+                        <div className="column">Thr <div>11am-11pm</div></div>
+                        <div className="column">Fri <div>11am-11pm</div></div>
+                        <div className="column">Sat <div>10am-11pm</div></div>
+                        <div className="column">Sun <div>10am-10pm</div></div>
                     </div>
                 </div>
-                <ContactForm />
             </div>
+            <ContactForm />
         </div>
     </div>);
 };
